2cdf5033f8b46766fc470d3aee8bf91ca5460e2b
Kim Sjöholm <kimsjooholm@gmail.com>
2020-08-28 11:03:44 +0200

    skapa flera tabeller

--- projekthanterare.js
+++ projekthanterare.js
@@ -8,15 +8,32 @@ class task {
     }
  
     make() {
+
+        
         var div = document.createElement('div');
         div.className = 'a';
         div.style.backgroundColor = "red";
-
-
+        
+        if ($(".a")[0]){
+            div.className = 'b';
+            div.style.backgroundColor = "green";
+        } else {
+            
+        }
+        if ($(".b")[0]){
+            div.className = 'c';
+            div.style.backgroundColor = "yellow";
+        } else {
+            
+        }
+       
         document.body.appendChild(div);
+
+        
     }
 }
 
+    
 window.onload = function () {
     document.getElementById("block").onclick = function () {
         newtask = new task("Kim", 1, "Blå");
