fd4c4e0ad7914df39c394adf26dd87ca08b41d41
Wiggly <w.n.richardson2001@gmail.com>
2020-09-02 13:15:20 +0200

    rework for main, not finished

--- projectCreator.js
+++ projectCreator.js
@@ -1,23 +1,37 @@
 var newtask;
 var newproject;
 
+var columnid = 0;
+var colums = new Array();
+var spacebetween = 200;
+
 class task {
-    constructor(name, id, color) {
+    constructor(name, id, color, order) {
         this.name = name;
         this.id = id;
         this.color = color;
+        this.order = order;
+        this.pos = 0;
+        this.element = document.body;
     }
 
-    make1() {
+    updatepos() {
+        
+        this.element = this.order;
+
+
+        this.pos = spacebetween * this.order;
+
+    }
+
+    make() {
         var div = document.createElement('div');
-        div.style.height = "200px";
-        div.style.width = "200px";
-        div.style.backgroundColor = "gray";
-        div.style.marginLeft = "50px";
-        div.style.marginTop = "50px";
-        div.style.cssFloat = "left";
-        div.innerHTML = "New Project";
-        div.id = "info";
+        div.className = 'dragable';
+        div.id = columnid;
+        columnid += 1;
+        div.style.backgroundColor = "#7495AB";
+        this.element = div;
+        div.innerHTML = "New Project ";
         var input = document.createElement('input');
         input.type = "text";
         input.name = "projectName";
@@ -26,6 +40,26 @@ class task {
         input2.type = "text";
         input2.name = "description";
         input2.value = "Description";
+        var btn = document.createElement('button')
+        btn.type = "button";
+        btn.value = "submit";
+        btn.innerHTML = "Sumbit";
+
+        if ($(".dragable")[0]) {
+            div.className = 'dragable';
+           
+
+        } else {
+
+        }
+        if ($('[className="dragable"]').length > 1) {
+            div.className = 'dragable';
+            div.style.backgroundColor = "#7495AB";
+        } else {
+
+        }
+
+        div.appendChild(btn);
         div.appendChild(input);
         div.appendChild(input2);
         PList.appendChild(div);
@@ -35,7 +69,7 @@ class task {
 window.onload = function () {
     document.getElementById("block").onclick = function () {
         newtask = new task("Kim", 1, "Blå");
-        newtask.make1();
+        newtask.make();
         
     }
 }
